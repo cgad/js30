eb464d01ff439b2a68fd9215ccffd30e8ccf3d8e
cgad <35946778+cgad@users.noreply.github.com>
2019-05-03 11:19:26 -0600

    Toggle military/standard time

--- 02-js-css-clock/index.js
+++ 02-js-css-clock/index.js
@@ -1,33 +1,67 @@
+let now;
+let seconds;
+let minutes;
+let hour;
+
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
+const analogTime = document.querySelector(".analogTime");
+const toggleButton = document.querySelector(".toggle");
+const analogSetting = document.querySelector(".setting");
 
-const sec = document.querySelector(".sec");
-const min = document.querySelector(".min");
-const hour = document.querySelector(".hour");
+toggleButton.addEventListener("click", function() {
+  if (toggleButton.classList.contains("military")) {
+    toggleButton.classList.remove("military");
+    toggleButton.innerHTML = "to military";
+    analogSetting.innerHTML = "STANDARD";
+  } else {
+    toggleButton.classList.add("military");
+    toggleButton.innerHTML = "to standard";
+    analogSetting.innerHTML = "MILITARY";
+  }
+});
 
-const analog = document.querySelector(".analog p");
+function setTime() {
+  now = new Date(); // new Date instance
+  seconds = now.getSeconds(); // get seconds of current minute
+  minutes = now.getMinutes();
+  hour = now.getHours();
 
-function setDate() {
-  const now = new Date(); // new Date instance
-  let seconds = now.getSeconds(); // get seconds of current minute
-  if (seconds < 10) seconds = "0" + seconds; // so doesn't show in analog as '1' instead of '01' etc.
   // turn seconds of minute into a degree
   // base 100 so scale 0% to 100% = 0 to 360 deg
   const secondsDegrees = (seconds / 60) * 360 + 90; // add 90 because we initially offset by 90deg to get the hands vertical by default, so now it's like as if it's still starting at it's original left-to-right orientation so you have to add 90 to match the offset we've already added
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  const minutes = now.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-  const hours = now.getHours();
-  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  const hourDegrees = (hour / 12) * 360 + (minutes / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
-  analog.innerHTML = `${hours}:${minutes}:${seconds}`;
+  // for analog
+  if (seconds < 10) seconds = `0${seconds}`;
+  if (minutes < 10) minutes = `0${minutes}`;
+
+  if (toggleButton.classList.contains("military")) {
+    analogTime.innerHTML = `${hour}:${minutes}:${seconds}`;
+  } else {
+    toStandard();
+  }
 }
 
-// setDate();
+// to standard time
+function toStandard() {
+  let identifier;
+  let stdHour = hour;
+  if (0 < hour && hour < 12) {
+    identifier = "AM";
+  } else {
+    identifier = "PM";
+    stdHour = hour - 12;
+  }
+  analogTime.innerHTML = `${stdHour}:${minutes}:${seconds} ${identifier}`;
+}
 
-setInterval(setDate, 1000);
+setTime();
+setInterval(setTime, 1000);
